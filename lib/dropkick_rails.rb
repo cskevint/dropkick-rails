e999790b4d3c80f290a5211d0c20f2eea3b0f85d
Kevin Trotter <cskevint@gmail.com>
2014-04-09 17:30:35 -0700

    updated to DropKick 1.4

--- lib/dropkick_rails.rb
+++
@@ -1,5 +0,0 @@
-require File.expand_path("../dropkick_rails/version", __FILE__)
-require File.expand_path("../dropkick_rails/engine", __FILE__)
-
-module DropkickRails
-end
--- lib/dropkick_rails/version.rb
+++
@@ -1,3 +0,0 @@
-module DropkickRails
-  VERSION = "0.0.1"
-end
